fcbb823c5eb3e39511cec03434f3f303de5367b5
Richard Davis <rdavis.bacs@gmail.com>
2014-06-30 20:42:08 -0400

    attach image spec

---
+++ spec/features/create_a_profile_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'create a profile' do
+
+  scenario 'user creates a profile with a photo' do
+    visit new_user_path
+    fill_in 'First name', with: 'Richard'
+    fill_in 'Last name', with: 'Davis'
+    fill_in 'Email', with: 'richard.davis@launchacademy.com'
+    attach_file 'Photo', 'lib/assets/images/richard.jpg'
+    click_on 'Create'
+
+    user = User.last
+    expect(page).to have_content('richard.davis@launchacademy.com')
+    expect(page).to have_css("img[src='#{user.photo_url}']")
+  end
+end
